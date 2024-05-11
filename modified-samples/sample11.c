abcf474d9ad7203e7455dce2f7abf95b127f23ab
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2024-05-11 16:45:02 +0800

    finished sample 11

---
+++ modified-samples/sample11.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<process.h>
+
+struct record //structure definition
+{
+    char name[50];
+    int age;
+    float wage;
+};
+
+void main(){ //begin main program
+    FILE *fp; //file declaration
+    
+    struct record myfriend; //structure declaration
+    char file[30];
+    int a;
+
+    printf("Enter filename: ");
+    gets(file);
+
+    fp = fopen(file, "rb"); //opening a file for reading only
+        if(fp == NULL){ 
+            printf("Error opening file");
+            exit(1);
+        }
+        
+    printf("All records\n");
+
+    //using fread only reads binary data, thus the used of fscanf
+    while (fscanf(fp, "%s %d %f", myfriend.name, &myfriend.age, &myfriend.wage) == 3) {
+        // display values
+        printf("\n%s %d %.2f", myfriend.name, myfriend.age, myfriend.wage);
+    }
+    
+    fclose(fp); //closing the file
+    getch();
+}
